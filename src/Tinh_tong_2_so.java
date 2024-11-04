bacc59fe39e8c9c824fdbfd86df728df8ab5c5ba
hoanh <hoanhphap582002@gmail.com>
2024-11-04 21:16:18 +0700

    tinh tong 2 so

---
+++ src/Tinh_tong_2_so.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Tinh_tong_2_so {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        long sum = a+b;
+        System.out.println(sum);
+        sc.close();
+    }
+
+}
